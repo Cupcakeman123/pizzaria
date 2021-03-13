12458c52b2087526b562d924b9ca592679e54bc0
Cupcakeman123 <75188459+Cupcakeman123@users.noreply.github.com>
2021-03-13 11:52:13 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var pizza_array = [];
+var pizza_array = ["bdc pizza", "halal chicken pizza", "gyro pizza"];
 
-var 
+var pizza_suggestions = "";
+
+document.getElementById("pre display").innerHTML = pizza_array;
 
 function submit()
 {
-    pizza_array.push()
+    pizza_suggestions = document.getElementById(name_of_pizza).innerHTML;
+
+    pizza_array.push(pizza_suggestions);
+    document.getElementById("display").innerHTML = pizza_array;
 }
